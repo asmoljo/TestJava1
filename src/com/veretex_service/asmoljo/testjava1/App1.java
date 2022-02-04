a4ab1f887c87e4c02bd2a57c2eeb7a48cc92f9ae
asmoljo <asmoljo@NBZG-46341.hpb.local>
2022-02-04 21:34:10 +0100

    initial commit

---
+++ src/com/veretex_service/asmoljo/testjava1/App1.java
@@ -0,0 +1,13 @@
+package com.veretex_service.asmoljo.testjava1;
+
+public class App1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bok");
+		
+		// TODO: ddadddds
+
+	}
+
+}
